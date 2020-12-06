06159e2d41122f4d0fe81a1469fbde5e131519f3
Seung Woo (Paul) Ji <wincow.ji@gmail.com>
2020-12-06 17:36:32 -0500

    Updated Assignment 6

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /*********************************************************************************
- * WEB322 – Assignment 05
+ * WEB322 – Assignment 06
  * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
  * of this assignment has been copied manually or electronically from any other source
  * (including 3rd party web sites) or distributed to other students.
